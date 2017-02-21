2ec06dad997d363c484a287563973395d1972979
andri lim <jangko128@gmail.com>
2017-02-21 08:33:10 +0700

    add trans_viewport

--- agg_conv_smooth_poly1.nim
+++ agg_conv_smooth_poly1.nim
@@ -1,22 +1,32 @@
-import agg_basics, agg_vcgen_smooth_poly1, agg_conv_adaptor_vcgen, agg_conv_curve
+import agg_basics, agg_vcgen_smooth_poly1, agg_conv_adaptor_vcgen
+import agg_conv_curve, agg_curves
 
 type
   ConvSmoothPoly1*[VertexSource] = object of ConvAdaptorVcgen[VertexSource, VcgenSmoothPoly1, NullMarkers]
-  base[VS] = ConvAdaptorVcgen[VS, VcgenSmoothPoly1, NullMarkers]
 
 proc initConvSmoothPoly1*[VS](vs: var VS): ConvSmoothPoly1[VS] =
-  base[VS](result).init(vs)
+  type base = ConvAdaptorVcgen[VS, VcgenSmoothPoly1, NullMarkers]
+  base(result).init(vs)
 
-proc smoothValue*[VS](self: var ConvSmoothPoly1[VS], v: float64) = base[VS](self).generator().smoothValue(v)
-proc smoothValue*[VS](self: ConvSmoothPoly1[VS]): float64 = base[VS](self).generator().smoothValue()
+proc smoothValue*[VS](self: var ConvSmoothPoly1[VS], v: float64) = 
+  type base = ConvAdaptorVcgen[VS, VcgenSmoothPoly1, NullMarkers]
+  base(self).generator().smoothValue(v)
+  
+proc smoothValue*[VS](self: ConvSmoothPoly1[VS]): float64 = 
+  type base = ConvAdaptorVcgen[VS, VcgenSmoothPoly1, NullMarkers]
+  base(self).generator().smoothValue()
 
-#type
-#  ConvSmoothPoly1Curve*[VertexSource] = object of ConvCurve[ConvSmoothPoly1[VertexSource]]
-#    mSmooth: ConvSmoothPoly1[VertexSource]
-#
-#proc initConvSmoothPoly1Curve*[VS](vs: var VS): ConvSmoothPoly1Curve[VS] =
-#  ConvCurve[ConvSmoothPoly1[VS]](result).init(result.mSmooth)
-#  self.mSmooth = initConvSmoothPoly1(vs)
-#
-#proc smoothValue*(self: var ConvSmoothPoly1Curve[VS], v: float64) = self.mSmooth.generator().smoothValue(v)
-#proc smoothValue*(self: ConvSmoothPoly1Curve[VS]): float64 = self.mSmooth.generator().smoothValue()
+type
+  ConvSmoothPoly1Curve*[VertexSource] = object of ConvCurve1[ConvSmoothPoly1[VertexSource], Curve3, Curve4]
+    mSmooth: ConvSmoothPoly1[VertexSource]
+
+proc initConvSmoothPoly1Curve*[VS](vs: var VS): ConvSmoothPoly1Curve[VS] =
+  type base = ConvCurve1[ConvSmoothPoly1[VS], Curve3, Curve4]
+  base(result).init(result.mSmooth)
+  result.mSmooth = initConvSmoothPoly1(vs)
+
+proc smoothValue*[VS](self: var ConvSmoothPoly1Curve[VS], v: float64) = 
+  self.mSmooth.generator().smoothValue(v)
+  
+proc smoothValue*[VS](self: ConvSmoothPoly1Curve[VS]): float64 = 
+  self.mSmooth.generator().smoothValue()
